a86669af3cd23674e3feaf29058d49708a712dbe
Ashik1704026 <ashiqurr343@gmail.com>
2021-09-17 21:16:43 +0600

    v1

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php echo "Hey man..." ?>
+<?php 
+echo "Hey man...";
+
+$con=mysqli_connect('sql5.freesqldatabase.com','sql5437992','uRqlHhGhpf','sql5437992') or die(mysqli_error());
+if($con)
+    echo "connected successfully";
+else
+    echo "not connected man...";
+?>
